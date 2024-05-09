83bc4fa2a21ed3a026dd4be3e69361eb0a728e76
aradhanasonii <aradhanashweta3@gmail.com>
2024-05-09 12:38:14 +0530

    Pushing the code

---
+++ src/test/java/Practice1/SampleProgram.java
@@ -0,0 +1,8 @@
+package Practice1;
+
+public class SampleProgram {
+	public static void main(String[] args) {
+		System.out.println("hiii");
+	}
+
+}
---
+++ src/test/java/Practice2/SampleProgram1.java
@@ -0,0 +1,8 @@
+package Practice2;
+
+public class SampleProgram1 {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
